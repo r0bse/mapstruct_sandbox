f5427cb46b119ca35fa5521dc8ddb348be7d9ef8
robert <robert.schroeder@dbfuhrpark.com>
2018-05-13 20:48:27 +0200

    added generated methods for equals, hashcode and toString

--- src/main/java/de/schroeder/mapstruct/person/boundary/PersonResource.java
+++ src/main/java/de/schroeder/mapstruct/person/boundary/PersonResource.java
@@ -1,6 +1,7 @@
 package de.schroeder.mapstruct.person.boundary;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 public class PersonResource {
 
@@ -21,5 +22,32 @@ public class PersonResource {
 
     public void setBirthday(ZonedDateTime birthday) {
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonResource{" +
+            "fullname='" + fullname + '\'' +
+            ", birthday=" + birthday +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PersonResource that = (PersonResource) o;
+        return Objects.equals(fullname, that.fullname) &&
+            Objects.equals(birthday, that.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(fullname, birthday);
     }
 }
